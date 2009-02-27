b09ab6e7bd51a65a35b35e4a4fdb1ea67f34d91c
ashbb <ashbbb@gmail.com>
2009-02-28 02:25:38 +0900

    deleted spaces from eBook name

--- tools/easy_ebook.rb
+++ tools/easy_ebook.rb
@@ -1,7 +1,7 @@
 # This is the line you will need to change per directions.
 module EasyEBook
   PATH = 'http://github.com/ashbb/shoes_tips_board/tree/master'
-  EBOOK = 'Shoes Tips Board'
+  EBOOK = 'ShoesTipsBoard'
   FLIP = '<h2>Table of contents</h2>'
   FLOP = '</ol>'
 end
